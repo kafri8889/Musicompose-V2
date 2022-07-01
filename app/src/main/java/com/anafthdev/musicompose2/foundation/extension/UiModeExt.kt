0b49b18ed3259e1a2da763717a093176cfca176f
kafri8889 <kafri8889@gmail.com>
2022-07-01 10:09:07 +0700

    app extension

---
+++ app/src/main/java/com/anafthdev/musicompose2/foundation/extension/UiModeExt.kt
@@ -0,0 +1,8 @@
+package com.anafthdev.musicompose2.foundation.extension
+
+import com.anafthdev.musicompose2.foundation.uimode.data.UiMode
+
+fun UiMode.isDark(): Boolean = this == UiMode.DARK
+fun UiMode.isLight(): Boolean = this == UiMode.LIGHT
+fun UiMode.isDynamicDark(): Boolean = this == UiMode.DYNAMIC_DARK
+fun UiMode.isDynamicLight(): Boolean = this == UiMode.DYNAMIC_LIGHT
