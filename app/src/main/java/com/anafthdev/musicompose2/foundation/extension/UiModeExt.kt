76bf1aa20130598fd505572f2a8b26c4314ce348
kafri8889 <kafri8889@gmail.com>
2022-07-01 16:46:12 +0700

    add new extension function

--- app/src/main/java/com/anafthdev/musicompose2/foundation/extension/UiModeExt.kt
+++ app/src/main/java/com/anafthdev/musicompose2/foundation/extension/UiModeExt.kt
@@ -6,3 +6,5 @@ fun UiMode.isDark(): Boolean = this == UiMode.DARK
 fun UiMode.isLight(): Boolean = this == UiMode.LIGHT
 fun UiMode.isDynamicDark(): Boolean = this == UiMode.DYNAMIC_DARK
 fun UiMode.isDynamicLight(): Boolean = this == UiMode.DYNAMIC_LIGHT
+fun UiMode.isInDarkTheme(): Boolean = isDark() or isDynamicDark()
+fun UiMode.isInLightTheme(): Boolean = isLight() or isDynamicLight()
