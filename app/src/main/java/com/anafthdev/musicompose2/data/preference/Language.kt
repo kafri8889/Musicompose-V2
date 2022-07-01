642726f2102c27e528a3de987a135b5f19e6c9dd
kafri8889 <kafri8889@gmail.com>
2022-07-01 10:12:59 +0700

    app preference

---
+++ app/src/main/java/com/anafthdev/musicompose2/data/preference/Language.kt
@@ -0,0 +1,6 @@
+package com.anafthdev.musicompose2.data.preference
+
+enum class Language(val lang: String) {
+	ENGLISH("en"),
+	INDONESIAN("id")
+}
---
+++ app/src/main/java/com/anafthdev/musicompose2/data/preference/Preference.kt
@@ -0,0 +1,9 @@
+package com.anafthdev.musicompose2.data.preference
+
+object Preference {
+
+	const val DYNAMIC_COLOR = "dynamic_color"
+	const val LANGUAGE = "language"
+	const val UI_MODE = "ui_mode"
+	
+}
