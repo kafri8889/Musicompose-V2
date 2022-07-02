0d1a2b3df893a06b0106df9bced8676850417869
kafri8889 <kafri8889@gmail.com>
2022-07-02 14:12:26 +0700

    display song list

--- app/src/main/java/com/anafthdev/musicompose2/feature/home/HomeScreen.kt
+++ app/src/main/java/com/anafthdev/musicompose2/feature/home/HomeScreen.kt
@@ -2,25 +2,44 @@ package com.anafthdev.musicompose2.feature.home
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.anafthdev.musicompose2.data.model.Song
 import com.anafthdev.musicompose2.feature.musicompose.LocalMusicomposeState
+import com.anafthdev.musicompose2.foundation.common.LocalSongController
+import com.anafthdev.musicompose2.foundation.uicomponent.SongItem
 
 @Composable
 fun HomeScreen() {
 	
+	val songController = LocalSongController.current
 	val musicomposeState = LocalMusicomposeState.current
 	
 	val viewModel = hiltViewModel<HomeViewModel>()
 	
 	LazyColumn(
 		modifier = Modifier
+			.statusBarsPadding()
 			.fillMaxSize()
 			.background(MaterialTheme.colorScheme.background)
 	) {
-	
+		
+		items(
+			items = musicomposeState.songs,
+			key = { song: Song -> song.audioID }
+		) { song ->
+			SongItem(
+				song = song,
+				isMusicPlaying = false,
+				onClick = {
+					songController?.play(song)
+				}
+			)
+		}
 	}
 }
