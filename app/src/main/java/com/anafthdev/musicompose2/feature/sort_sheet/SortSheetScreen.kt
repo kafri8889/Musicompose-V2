ba4ad36666ada7bb1e709690d31dfa50568e7995
kafri8889 <kafri8889@gmail.com>
2022-07-04 12:55:26 +0700

    move `SortItem`

--- app/src/main/java/com/anafthdev/musicompose2/feature/sort_sheet/SortSheetScreen.kt
+++ app/src/main/java/com/anafthdev/musicompose2/feature/sort_sheet/SortSheetScreen.kt
@@ -1,10 +1,8 @@
 package com.anafthdev.musicompose2.feature.sort_sheet
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -23,7 +21,7 @@ import com.anafthdev.musicompose2.data.SortArtistOption
 import com.anafthdev.musicompose2.data.SortSongOption
 import com.anafthdev.musicompose2.data.SortType
 import com.anafthdev.musicompose2.foundation.extension.optionToString
-import com.anafthdev.musicompose2.foundation.theme.Inter
+import com.anafthdev.musicompose2.foundation.uicomponent.SortItem
 
 @Composable
 fun SortSheetScreen(
@@ -95,41 +93,5 @@ fun SortSheetScreen(
 				}
 			}
 		}
-	}
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun SortItem(
-	text: String,
-	selected: Boolean,
-	modifier: Modifier = Modifier,
-	onClick: () -> Unit
-) {
-	
-	Row(
-		verticalAlignment = Alignment.CenterVertically,
-		modifier = modifier
-			.clickable(onClick = onClick)
-	) {
-		Text(
-			text = text,
-			style = MaterialTheme.typography.titleSmall.copy(
-				fontFamily = Inter
-			),
-			modifier = Modifier
-				.wrapContentWidth(Alignment.Start)
-				.padding(top = 14.dp, bottom = 14.dp, start = 16.dp)
-		)
-		
-		Spacer(modifier = Modifier.weight(1f))
-		
-		RadioButton(
-			selected = selected,
-			onClick = onClick,
-			modifier = Modifier
-				.wrapContentSize(Alignment.CenterEnd)
-				.padding(end = 16.dp)
-		)
 	}
 }
