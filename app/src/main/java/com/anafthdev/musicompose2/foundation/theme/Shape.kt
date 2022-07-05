d7d82a382ffa9febed4a5f6649ebc2d86a9e8189
kafri8889 <kafri8889@gmail.com>
2022-07-05 12:01:14 +0700

    add new shape `no_shape`

--- app/src/main/java/com/anafthdev/musicompose2/foundation/theme/Shape.kt
+++ app/src/main/java/com/anafthdev/musicompose2/foundation/theme/Shape.kt
@@ -3,3 +3,4 @@ package com.anafthdev.musicompose2.foundation.theme
 import androidx.compose.foundation.shape.RoundedCornerShape
 
 val circle = RoundedCornerShape(100)
+val no_shape = RoundedCornerShape(0)
