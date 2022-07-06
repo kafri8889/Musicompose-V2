eb3faacac47bbc18e017050ded0888c0b7d0ec51
kafri8889 <kafri8889@gmail.com>
2022-07-06 14:23:31 +0700

    fix playlist not updated in `PlaylistScreen`

--- app/src/main/java/com/anafthdev/musicompose2/feature/playlist/playlist/environment/PlaylistEnvironment.kt
+++ app/src/main/java/com/anafthdev/musicompose2/feature/playlist/playlist/environment/PlaylistEnvironment.kt
@@ -4,9 +4,12 @@ import com.anafthdev.musicompose2.data.model.Playlist
 import com.anafthdev.musicompose2.data.repository.Repository
 import com.anafthdev.musicompose2.foundation.di.DiName
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 import javax.inject.Named
 
@@ -18,14 +21,30 @@ class PlaylistEnvironment @Inject constructor(
 	private val _playlist = MutableStateFlow(Playlist.default)
 	private val playlist: StateFlow<Playlist> = _playlist
 	
+	private val _playlistID = MutableStateFlow(Playlist.default.id)
+	private val playlistID: StateFlow<Int> = _playlistID
+	
+	init {
+		CoroutineScope(dispatcher).launch {
+			combine(
+				playlistID,
+				repository.getPlaylists()
+			) { mPlaylistID, playlists ->
+				mPlaylistID to playlists
+			}.collect { (mPlaylistID, playlists) ->
+				_playlist.emit(
+					playlists.find { it.id == mPlaylistID } ?: Playlist.default
+				)
+			}
+		}
+	}
+	
 	override fun getPlaylist(): Flow<Playlist> {
 		return playlist
 	}
 	
 	override suspend fun setPlaylist(playlistID: Int) {
-		_playlist.emit(
-			repository.getPlaylist(playlistID) ?: Playlist.justPlayed
-		)
+		_playlistID.emit(playlistID)
 	}
 	
 }
