3bf8f93f070b917b58afc040d77d8c7395759e9f
kafri8889 <kafri8889@gmail.com>
2022-07-09 16:24:47 +0700

    toast in main thread

--- app/src/main/java/com/anafthdev/musicompose2/foundation/extension/AnyExt.kt
+++ app/src/main/java/com/anafthdev/musicompose2/foundation/extension/AnyExt.kt
@@ -2,6 +2,8 @@ package com.anafthdev.musicompose2.foundation.extension
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.os.Handler
+import android.os.Looper
 import android.widget.Toast
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
@@ -12,7 +14,9 @@ import androidx.compose.ui.platform.LocalContext
  * @author kafri8889
  */
 fun Any.toast(context: Context, length: Int = Toast.LENGTH_SHORT) {
-	Toast.makeText(context, this.toString(), length).show()
+	Handler(Looper.getMainLooper()).post {
+		Toast.makeText(context, this.toString(), length).show()
+	}
 }
 
 /**
